0060b03ef3f1f1cc47768fd8d42dd01282ff2837
Ziszel <the8thnoteofficial@gmail.com>
2023-04-12 14:49:11 +0100

    Updated MainMenu with music, and added music to MainScene.

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -5,14 +6,18 @@ using UnityEngine.UI;
 public class MenuManager : MonoBehaviour
 {
     public Button beginBtn;
+    private AudioSource _flavour;
 
     private void Start()
     {
+        _flavour = GetComponent<AudioSource>();
         beginBtn.onClick.AddListener(BeginClicked);
     }
 
     private void BeginClicked()
     {
+        // Once shorter audio track is sourced this will be re-enabled
+        //StartCoroutine(PlayFlavourTrack());
         LoadMainScene();
     }
 
@@ -20,4 +25,21 @@ public class MenuManager : MonoBehaviour
     {
         SceneManager.LoadScene("MainScene");
     }
+    
+    IEnumerator PlayFlavourTrack()
+    {
+        // Begin playing the track
+        _flavour.Play();
+        // Wait until the track is finished before loading the main scene.
+        // Since the track is very short this will look more like a sound effect but with no cutting off.
+        while (_flavour.isPlaying)
+        {
+            // In here, an animation could be ran at the same time (perhaps fading to black?)
+            yield return null;
+        }
+
+        // wait until track has finished to load the main game
+        LoadMainScene();
+    }
+
 }
